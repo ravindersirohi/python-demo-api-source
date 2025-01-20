e43e2fc5569c223a2740edc9b1b671e20b3a7549
Ravinder Sirohi <ravinder@hovergen.com>
2025-01-20 21:03:27 +0000

    Draft API is up

--- api-source/app.py
+++ api-source/app.py
@@ -2,6 +2,14 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Flask API is up!"
+
 @app.route('/health-check')
-def health():
-    return "App is up and running."
+def health_check():
+    return "Hello, I am healthy!"
+
+if __name__ == '__main__':
+    # app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
