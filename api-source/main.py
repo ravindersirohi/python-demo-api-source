9227086f60f4cdc9d14f5e3bbbdc7c5ba3bc770b
Ravinder Sirohi <ravinder@hovergen.com>
2025-01-21 21:50:57 +0000

    old file added.

--- api-source/main.py
+++ api-source/main.py
@@ -1,49 +1,49 @@
-# import os
-# from flask import Flask,render_template, jsonify, request
-
-# app = Flask(__name__)
-
-# static_list = []
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# @app.route('/health-check')
-# def health_check():
-#     return "Hello, I am healthy!"
-
-# @app.route('/list-items')
-# def list_items():
-#     return jsonify(static_list)
-
-# @app.route('/add-item', methods=['POST'])
-# def add_item():
-#     name = request.json['name']
-#     if name is not None and name !='':
-#         static_list.append({"name":name})
-#         return "Item added into the list!"
-#     else:
-#         return "Unable to add item!"
-
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
-#     # app.run(debug=True)
-
-from flask import Flask
 import os
+from flask import Flask,render_template, jsonify, request
 
 app = Flask(__name__)
 
+static_list = []
+
 @app.route('/')
 def main():
-    return "Hello, World!"
+    return render_template('index.html')
 
-@app.route('/health')
-def health():
-    return "Healthy", 200
+@app.route('/health-check')
+def health_check():
+    return "Hello, I am healthy!"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+@app.route('/list-items')
+def list_items():
+    return jsonify(static_list)
+
+@app.route('/add-item', methods=['POST'])
+def add_item():
+    name = request.json['name']
+    if name is not None and name !='':
+        static_list.append({"name":name})
+        return "Item added into the list!"
+    else:
+        return "Unable to add item!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
+    # app.run(debug=True)
+
+# from flask import Flask
+# import os
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def main():
+#     return "Hello, World!"
+
+# @app.route('/health')
+# def health():
+#     return "Healthy", 200
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8080))
+#     app.run(host="0.0.0.0", port=port)
 
